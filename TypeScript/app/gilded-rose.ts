4c691684dfad30e3c2d73572fc815f5c5b08b678
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-06-21 16:19:45 +0100

    consolidated aged brie code to switch function, deleted superceded code

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -26,18 +26,26 @@ export class GildedRose {
       switch(this.items[i].name){
         case 'Aged Brie':
           //Aged Brie code
+          this.items[i].sellIn = this.items[i].sellIn - 1;
+          this.items[i].quality = this.items[i].quality + 1;
+          if (this.items[i].sellIn < 0){
+            this.items[i].quality ++;
+          }
+          if (this.items[i].quality > 50) {
+            this.items[i].quality = 50
+          }
           break;
         case 'Backstage passes to a TAFKAL80ETC concert':
           //backstage passes code
           let addedQuality = 0;
           if (this.items[i].sellIn < 6){
-            addedQuality = 2;
+            addedQuality = 3;
           } else if (this.items[i].sellIn < 11) {
-            addedQuality = 1;
+            addedQuality = 2;
           } else {
             //this (may arguably) be needed only once the section below marked BACKSTAGE HERE, which adds 1 to *all* quality increasing
             //products, is removed
-            //addedQuality = 1;
+            addedQuality = 1;
           }
           this.items[i].quality = this.items[i].quality + addedQuality;
           if (this.items[i].quality > 50) {
@@ -54,36 +62,9 @@ export class GildedRose {
           break;
 
       }
-      //nested if set 1 - reduces quality by 1 of everything but aged brie, backstage passes and sulfuras, provided quality > 0
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        /*if (this.items[i].quality > 0) {          
-          this.items[i].quality = this.items[i].quality - 1
-        }*/
-      } else {
-        if (this.items[i].quality < 50) {
-          //BACKSTAGE HERE
-          //this adds 1 to all the quality increasing products
-          this.items[i].quality = this.items[i].quality + 1
-          /*if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            //this adds one
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-              //this.items[i].quality = this.items[i].quality + 1
-            }
-            //this adds another 1 as quality increases faster (upto a ceiling of 50)
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-              //this.items[i].quality = this.items[i].quality + 1
-            }
-          }*/
-        }
-      }
+      
       //having assigned the quality this reduces sellin by 1 for everything except sulfuras
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros' && this.items[i].name != 'Aged Brie') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       //excepting aged brie, backstage passes and sulfuras, this reduces quality by 1, again (since quality increases twice as fast after sell by date)
@@ -101,12 +82,7 @@ export class GildedRose {
             //this.items[i].quality = this.items[i].quality - this.items[i].quality
             this.items[i].quality = 0;
           }
-        } else {
-          //this increases the quality of brie by 1 upto a threshold of 50
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
+        } 
       }
     }
 
