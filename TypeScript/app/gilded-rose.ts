568be8239e330fa3e42876b49ea2dc8bd76d832a
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-06-21 17:38:03 +0100

    transferred code decrementing quality by 1 again for standard goods after sell by into switch function

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -62,23 +62,12 @@ export class GildedRose {
           if (this.items[i].quality > 0) {          
             this.items[i].quality = this.items[i].quality - 1
           }
+          if (this.items[i].quality > 0 && this.items[i].sellIn < 0) {          
+            this.items[i].quality = this.items[i].quality - 1
+          }
           break;
 
-      }
-      
-      
-      //excepting aged brie, backstage passes and sulfuras, this reduces quality by 1, again (since quality increases twice as fast after sell by date)
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          }          
-        } 
-      }
+      }      
     }
 
     return this.items;
