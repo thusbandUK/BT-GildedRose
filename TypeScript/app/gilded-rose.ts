7a62d286d7a917b2ccaecc3d27836c9e583bc68c
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-06-21 18:37:49 +0100

    updated default switch case to double quality decrement for Conjured products

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -21,23 +21,26 @@ export class GildedRose {
     
 
     for (let i = 0; i < this.items.length; i++) {
+      //This decrements sellIn for everything but Sulfuras - which never ages - and backstage passes, which decrements within below function
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
 
       switch(this.items[i].name){
         case 'Aged Brie':
-          //Aged Brie code          
+          //Adds 1 to quality
           this.items[i].quality = this.items[i].quality + 1;
+          //Adds an additional 1 to quality beyond sell by date
           if (this.items[i].sellIn < 0){
             this.items[i].quality ++;
           }
+          //caps quality at 50
           if (this.items[i].quality > 50) {
             this.items[i].quality = 50
           }
           break;
         case 'Backstage passes to a TAFKAL80ETC concert':
-          //backstage passes code
+          //Assigns value of 3, 2 or 1 to add to quality depending on sellIn 
           let addedQuality = 0;
           if (this.items[i].sellIn < 6){
             addedQuality = 3;
@@ -46,24 +49,49 @@ export class GildedRose {
           } else {            
             addedQuality = 1;
           }
+          //add assigned value to quality
           this.items[i].quality = this.items[i].quality + addedQuality;
+          //caps quality at 50
           if (this.items[i].quality > 50) {
             this.items[i].quality = 50
           }
+          //decrements sellIn
           this.items[i].sellIn = this.items[i].sellIn - 1;
           if (this.items[i].sellIn < 0){
             this.items[i].quality = 0;
           }
           break;
         case 'Sulfuras, Hand of Ragnaros':
-          //sulfuras code
+          //no code required as no changes ever made to Sulfuras
           break;
         default:
-          if (this.items[i].quality > 0) {          
-            this.items[i].quality = this.items[i].quality - 1
+          //Conjured items lose 2 or 4 from quality, depending on sellIn
+          if (this.items[i].name.includes('Conjured')){            
+            
+            if (this.items[i].quality > 1){
+              this.items[i].quality = this.items[i].quality - 2;
+            } else if (this.items[i].quality === 1){
+              this.items[i].quality = this.items[i].quality - 1;
+            }
+           if (this.items[i].sellIn < 0){
+
+            if (this.items[i].quality > 1){
+              this.items[i].quality = this.items[i].quality - 2;
+            } else if (this.items[i].quality === 1){
+              this.items[i].quality= this.items[i].quality - 1;
+            }
           }
-          if (this.items[i].quality > 0 && this.items[i].sellIn < 0) {          
-            this.items[i].quality = this.items[i].quality - 1
+            
+          } 
+          //default non-Conjured items lose 1 or 2 from quality, depending on sellIn
+          else {
+
+            if (this.items[i].quality > 0) {          
+              this.items[i].quality = this.items[i].quality - 1
+            }
+            if (this.items[i].quality > 0 && this.items[i].sellIn < 0) {          
+              this.items[i].quality = this.items[i].quality - 1            
+            }
           }
           break;
 
