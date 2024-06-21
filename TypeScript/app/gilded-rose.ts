21f784b20dd839bb31757ca84448ce2418cb8bb9
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-06-21 14:52:42 +0100

    added explanatory notes and simplified the term which sets backstage pass quality to zero after sell by date

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -23,33 +23,38 @@ export class GildedRose {
     for (let i = 0; i < this.items.length; i++) {
       
 
+      //nested if set 1 - reduces quality by 1 of everything but aged brie, backstage passes and sulfuras, provided quality > 0
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        if (this.items[i].quality > 0) {
-          /*if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }*/
+        if (this.items[i].quality > 0) {          
           this.items[i].quality = this.items[i].quality - 1
         }
       } else {
         if (this.items[i].quality < 50) {
+          //this adds 1 to all the quality increasing products
           this.items[i].quality = this.items[i].quality + 1
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+            //this adds one
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1
               }
+              //this.items[i].quality = this.items[i].quality + 1
             }
+            //this adds another 1 as quality increases faster (upto a ceiling of 50)
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1
               }
+              //this.items[i].quality = this.items[i].quality + 1
             }
           }
         }
       }
+      //having assigned the quality this reduces sellin by 1 for everything except sulfuras
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
+      //excepting aged brie, backstage passes and sulfuras, this reduces quality by 1, again (since quality increases twice as fast after sell by date)
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -58,10 +63,14 @@ export class GildedRose {
                 this.items[i].quality = this.items[i].quality - 1
               }
             }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
+          }
+          // this reduces to zero the quality of the backstage passes, once the concert has passed (ie sell by dated passed)
+          else {
+            //this.items[i].quality = this.items[i].quality - this.items[i].quality
+            this.items[i].quality = 0;
           }
         } else {
+          //this increases the quality of brie by 1 upto a threshold of 50
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1
           }
