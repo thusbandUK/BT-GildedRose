8114a8d48fab8bf074deb8d816a58e10fdbeaf03
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-06-21 14:33:25 +0100

    set 1 nested if statements moved sulfuras into opening if clause

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -18,12 +18,17 @@ export class GildedRose {
   }
 
   updateQuality() {
+    
+
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      
+
+      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          /*if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1
-          }
+          }*/
+          this.items[i].quality = this.items[i].quality - 1
         }
       } else {
         if (this.items[i].quality < 50) {
